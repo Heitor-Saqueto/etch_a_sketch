90b01574045bbb55bb917dbecaa4efd10ae4c777
Heitor Saqueto <heitorsaqueto@hotmail.com>
2023-06-30 19:00:38 -0300

    project_done

---
+++ main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+
+terry = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    terry.forward(10)
+
+
+def move_backwards():
+    terry.backward(10)
+
+
+def turn_right():
+    terry.right(10)
+
+
+def turn_lef():
+    terry.left(10)
+
+
+def clear():
+    terry.penup()
+    terry.home()
+    terry.clear()
+    terry.pendown()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="a", fun=turn_lef)
+screen.onkey(key="c", fun=clear)
+
+
+screen.exitonclick()
+
